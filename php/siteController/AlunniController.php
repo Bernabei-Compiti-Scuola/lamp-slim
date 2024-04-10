1b63c966fe3a274fb091118503a5dffd9ba78535
iacopo <bernabei.iacopo@itismeucci.com>
2024-04-10 12:11:20 +0200

    implemented CRUD operations for alunni

--- php/siteController/AlunniController.php
+++ php/siteController/AlunniController.php
@@ -15,23 +15,25 @@ class SiteController
     function get_alunni_by_id(Request $request, Response $response, $args) 
     {
         $mysqli_connection = new MySQLi('my_db', 'root', 'ciccio', 'alunni');
-        $result = $mysqli_connection->query("SELECT * FROM alunni");
+        $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = $args[id]");
         $results = $result->fetch_all(MYSQLI_ASSOC);
         $response->getBody()->write(json_encode($results));
         return $response->withHeader("Content-type", "application/json")->withStatus(201);
     }
+    //inserisce nuovo alunno
     function post_alunni(Request $request, Response $response, $args) 
     {
         $mysqli_connection = new MySQLi('my_db', 'root', 'ciccio', 'alunni');
-        $result = $mysqli_connection->query("SELECT * FROM alunni");
+        $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome) values ($args[nome], $args[cognome])");
         $results = $result->fetch_all(MYSQLI_ASSOC);
         $response->getBody()->write(json_encode($results));
         return $response->withHeader("Content-type", "application/json")->withStatus(201);
     }
+    //sostituisci dati alunno
     function put_alunni_by_id(Request $request, Response $response, $args) 
     {
         $mysqli_connection = new MySQLi('my_db', 'root', 'ciccio', 'alunni');
-        $result = $mysqli_connection->query("SELECT * FROM alunni");
+        $result = $mysqli_connection->query("UPDATE TABLE alunni SET nome = $args[nome], cognome = $args[cognome] WHERE id = $args[id]");
         $results = $result->fetch_all(MYSQLI_ASSOC);
         $response->getBody()->write(json_encode($results));
         return $response->withHeader("Content-type", "application/json")->withStatus(201);
@@ -39,7 +41,7 @@ class SiteController
     function delete_alunni_by_id(Request $request, Response $response, $args) 
     {
         $mysqli_connection = new MySQLi('my_db', 'root', 'ciccio', 'alunni');
-        $result = $mysqli_connection->query("SELECT * FROM alunni");
+        $result = $mysqli_connection->query("DELETE FROM alunni WHERE id = $args[id]");
         $results = $result->fetch_all(MYSQLI_ASSOC);
         $response->getBody()->write(json_encode($results));
         return $response->withHeader("Content-type", "application/json")->withStatus(201);
